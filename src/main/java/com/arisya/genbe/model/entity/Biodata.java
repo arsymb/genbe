ecdd12613e51034060a3adcd9301587a966f1883
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-08 22:54:01 +0700

    Add person, biodata, pendidikan entity

---
+++ src/main/java/com/arisya/genbe/model/entity/Biodata.java
@@ -0,0 +1,68 @@
+package com.arisya.genbe.model.entity;
+
+import java.util.Date;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "t_biodata")
+public class Biodata {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_bio")
+	private Integer idBio;
+
+	@Column(name = "nohp", length = 16)
+	private String noHp;
+
+	@Column(name = "tanggal_lahir", nullable = false)
+	private java.sql.Date tglLahir;
+
+	@Column(name = "tempat_lahir", length = 50)
+	private String tmptLahir;
+
+	@OneToOne
+	@JoinColumn(name = "idperson", unique = true, nullable = false)
+	private Person person;
+
+	public Integer getIdBio() {
+		return idBio;
+	}
+
+	public void setIdBio(Integer idBio) {
+		this.idBio = idBio;
+	}
+
+	public String getNoHp() {
+		return noHp;
+	}
+
+	public void setNoHp(String noHp) {
+		this.noHp = noHp;
+	}
+
+	public Date getTglLahir() {
+		return tglLahir;
+	}
+
+	public void setTglLahir(Date date) {
+		this.tglLahir = new java.sql.Date(date.getTime());
+	}
+
+	public String getTmptLahir() {
+		return tmptLahir;
+	}
+
+	public void setTmptLahir(String tmptLahir) {
+		this.tmptLahir = tmptLahir;
+	}
+
+	public Person getPerson() {
+		return person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+}

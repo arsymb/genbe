43bb1673e20089b5bc3af1cff5ddd3cff334d31f
DESKTOP-I7DKLCJ\arisyamb <arsymnb@gmail.com>
2020-09-08 22:55:33 +0700

    Add statusdto2

---
+++ src/main/java/com/arisya/genbe/model/dto/StatusDto2.java
@@ -0,0 +1,32 @@
+package com.arisya.genbe.model.dto;
+
+public class StatusDto2 {
+	private String status;
+	private String message;
+	private FullDto data;
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public FullDto getData() {
+		return data;
+	}
+
+	public void setData(FullDto data) {
+		this.data = data;
+	}
+
+}
